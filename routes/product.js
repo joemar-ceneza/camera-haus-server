8fe843204a07960a1c8e06974616ef2dab2f7aa3
joemar-ceneza <joemar.ceneza@gmail.com>
2024-11-02 02:17:51 +0800

    Add product creation endpoint with additional fields: description, regularPrice, isNewProduct, quantity and category.

--- routes/product.js
+++ routes/product.js
@@ -8,11 +8,23 @@ const router = express.Router();
 // create new product with image upload
 router.post("/", uploadProductImage.single("image"), async (req, res) => {
   try {
-    const { title } = req.body;
+    const {
+      title,
+      description,
+      regularPrice,
+      isNewProduct,
+      quantity,
+      category,
+    } = req.body;
     const imageUrl = req.file ? req.file.path : undefined;
     const newProduct = new Product({
-      title,
       image: imageUrl,
+      title,
+      description,
+      regularPrice,
+      isNewProduct,
+      quantity,
+      category,
     });
     const savedProduct = await newProduct.save();
     res.status(201).json(savedProduct);
@@ -46,7 +58,40 @@ router.put(
     try {
       const { title } = req.body;
       const imageUrl = req.file ? req.file.path : undefined;
-    } catch (error) {}
+
+      if (imageUrl) {
+        if (product.image) {
+          const publicId = product.image
+            .split("/")
+            .slice(-4)
+            .join("/")
+            .split(".")[0];
+          try {
+            await cloudinary.uploader.destroy(publicId);
+          } catch (error) {
+            console.error("Error deleting old image from Cloudinary", error);
+            return res
+              .status(500)
+              .json({ error: "Failed to delete old image from Cloudinary" });
+          }
+        }
+      }
+      const updateProduct = await product.save();
+      res.json(updateProduct);
+    } catch (error) {
+      if (req.file && req.file.path) {
+        const publicId = req.file.filename.split(".")[0];
+        try {
+          await cloudinary.uploader.destroy(publicId);
+        } catch (error) {
+          console.error(
+            "Error deleting new image from Cloudinary after update failed: ",
+            error
+          );
+        }
+      }
+      res.status(400).json({ error: error.message });
+    }
   })
 );
 

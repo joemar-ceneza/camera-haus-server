8e311e7938d42bae68d767d2e79cb8f1d5bdff6f
joemar-ceneza <joemar.ceneza@gmail.com>
2024-11-04 01:47:33 +0800

    Feat: Add Category model with image, title, slug and products references

---
+++ models/category.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      maxlength: 50,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// pre-save hook to automatically generate a slug from the name
+categorySchema.pre("save", function (next) {
+  if (this.isModified("title")) {
+    this.slug = this.title
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "");
+  }
+  next();
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
